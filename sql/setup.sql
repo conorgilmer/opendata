89e6fcbf5d594a84f993888366af2899b2fde6e2
Conor Gilmer <conor.gilmer@gmail.com>
2015-06-13 15:06:42 +0100

    sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,8 +2,10 @@ create table urllinks  (
     id int not null auto_increment primary key,
     first  DATE,
     last   DATE,
-    code   varchar(5),
-    url    varchar(25),
+    code   varchar(20),
+    url    varchar(75),
+    tblname    varchar(25),
+    source    varchar(25),
     enable int
 );
 
@@ -13,4 +15,9 @@ create table download (
     amount double(4,2)
 );
 
-
+CREATE TABLE `opendata`.`users` (
+`user_id` INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`first_name` VARCHAR( 25 ) NOT NULL ,
+`last_name` VARCHAR( 25 ) NOT NULL ,
+`user_city` VARCHAR( 45 ) NOT NULL
+) ENGINE = InnoDB;
