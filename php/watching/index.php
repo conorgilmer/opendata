7e9f4087616ca158f0bc7b628e9b3d100ecb622a
Conor Gilmer <conor.gilmer@gmail.com>
2015-06-17 21:53:26 +0100

    typo

--- php/watching/index.php
+++ php/watching/index.php
@@ -24,7 +24,7 @@ $res=$con->select($table);
     <table align="center">
     <tr>
     <th colspan="3"><a href="add_code.php">Add Data Code</a></th>
-    <th colspan="3"><a href="index.php">List Data</a></th>
+    <th colspan="5"><a href="index.php">List Data</a></th>
     </tr>
     <tr>
     <th>Code</th>
@@ -32,7 +32,7 @@ $res=$con->select($table);
     <th>Table</th>
     <th>Start</th>
     <th>Last</th>
-    <th>Action</th>
+    <th colspan="3">Actions</th>
     </tr>
     <?php
 	while($row=mysql_fetch_row($res))
@@ -45,6 +45,8 @@ $res=$con->select($table);
             <td><?php echo $row[4]; ?></td>
             <td><?php echo $row[5]; ?></td>
             <td><a href="graph.php?id=<?php echo $row[0]; ?>">Graph</a></td>
+            <td><a href="up.php?id=<?php echo $row[0]; ?>">Upload</a></td>
+            <td><a href="down.php?id=<?php echo $row[0]; ?>">CSV</a></td>
             </tr>
             <?php
 	}
